4cff56bf4f1bf771f7edc8ab9ae0bcad32cb2551
Audumber <audumberchaudhari3000@gmail.com>
2023-12-30 10:42:38 +0530

    done

--- routes/hrdashboard_hiring.js
+++ routes/hrdashboard_hiring.js
@@ -42,8 +42,9 @@ router.get("/dashboard_new_hiring", isLoggedIn, async function (req, res) {
     }
   });
 
-  selected_interns = await Interninfo_final.find({ Accepted: "Yes", Selected: "Yes", Rejected: "No", Completed: "No" }, function (err, one_detail) {
+  selected_interns = await Interninfo_final.find({}, function (err, one_detail) {
     for (var i = 0; i < one_detail.length; i++) {
+      // console.log(one_detail[i].Email+","+one_detail[i].Name)
     
     }
     if (err) {
@@ -221,7 +222,6 @@ router.post("/selection_action", isLoggedIn, async function (req, res) {
       });
 
       sms_req.end(function (res) {
-        if (res.error) throw new Error(res.error);
 
         console.log(res.body);
       });
