42144912a595e9d5b4ed428272abbe03eb6767fc
Audumber <audumberchaudhari3000@gmail.com>
2024-05-28 08:36:49 +0530

    done

--- app.js
+++ app.js
@@ -222,6 +222,8 @@ function isLoggedIn(req, res, next) {
   res.redirect("/login");
 }
 
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
